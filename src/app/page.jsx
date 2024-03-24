cf247aca6fc8203a9ba90eca01febf91b0486c8b
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-03-25 01:21:04 +0600

    nextJS blog website setup

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h1 className='bg-purple-500 text-white p-4 text-4xl max-w-screen-md mx-auto rounded-md text-center'>Blog V Home Page</h1>
+        </div>
+    );
+};
+
+export default page;
